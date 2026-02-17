3aa902df3bc8e852effa3a3ca516b23b438c4d51
Pr0xximity_999 <tomsomgamer999@gmail.com>
2026-02-17 18:42:48 +0100

    improvement of function syntax

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import random
 import json
 
+import paho.mqtt as mqtt
 from paho.mqtt import client as mqtt_client
 
-# Load json config
-with open("config.json", "r") as file:
-    config = json.load(file)
+def on_connect(client:mqtt_client.Client, userdata:any, flags:Dict[str, any], reason_code:mqtt.reasoncodes.ReasonCodes):
+    if reason_code.id == 0:
+        print(f"Connected to MQTT Broker with cient {client._client_id}.")
+    else:
+        print(f"Failed to connect, return code {reason_code}")
+
+def create_client(client_id, broker_address, broker_port=1883) -> mqtt_client:
     
-# Generate a Client ID with the subscribe prefix.
-client_id = "mqtt-dsmr-parser"
+    # Create client
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2, client_id)
 
-def connect_mqtt(address, port) -> mqtt_client:
-    def on_connect(client, userdata, flags, reason_code):
-        if reason_code == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", reason_code)
-
-    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
-    # client.username_pw_set(username, password)
+    # Bind connection function bind and connect
     client.on_connect = on_connect
-    client.connect(address, port)
+    client.connect(broker_address, broker_port)
     return client
 
 
@@ -33,7 +30,13 @@ def subscribe(client: mqtt_client):
 
 
 def run():
-    client = connect_mqtt()
+    # Load json config
+    with open("config.json", "r") as file:
+        config = json.load(file)
+        
+    # Generate a Client ID with the subscribe prefix.
+    client_id = "mqtt-dsmr-parser"
+    client = create_client()
     subscribe(client)
     client.loop_forever()
 
